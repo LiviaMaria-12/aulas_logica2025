c608b65bdb9368e85dbce79f09acd9bf740e5178
Livia Maria <livia.m.nogueira@aluno.senai.br>
2025-04-29 12:14:43 -0300

    Correção da Atividade 10 e Exercícios de Revisão

--- Exercícios/Exercicios_Revisao/Ex13.js
+++ Exercícios/Exercicios_Revisao/Ex13.js
@@ -1,3 +1,12 @@
 function NOTAS(nr1, nr2, nr3, nr4){
-    
+    let media = (nr1 + nr2 + nr3 + nr4) / 4
+    if(media > 7){
+        console.log(`Média: ${media} - APROVADO!`)
+    }else if(media <= 7 && media > 5){
+        console.log(`Média: ${media} - RECUPERAÇÃO!`)
+    }else{
+        console.log(`Média: ${media} - REPROVADO!`)
+    }
 }
+
+NOTAS(10, 10, 9, 9)
